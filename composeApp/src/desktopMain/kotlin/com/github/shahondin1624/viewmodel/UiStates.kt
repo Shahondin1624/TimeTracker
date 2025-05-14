41e306d3712f251db86227c11566aa2d8924a57e
Tobias Addicks <65025450+Shahondin1624@users.noreply.github.com>
2025-05-14 19:17:12 +0200

    Add dark mode (#4)
    * Save pending tracking on exit & add custom icons to app
    
    * Add dark mode and more details

--- composeApp/src/desktopMain/kotlin/com/github/shahondin1624/viewmodel/UiStates.kt
+++ composeApp/src/desktopMain/kotlin/com/github/shahondin1624/viewmodel/UiStates.kt
@@ -3,5 +3,6 @@ package com.github.shahondin1624.viewmodel
 import com.github.shahondin1624.Stories
 
 data class TimeTrackerUiState(
-    val stories: Stories = Stories()
+    val stories: Stories = Stories(),
+    val isDarkMode: Boolean = true
 )
